440d9a1c52cc48f464cc30a7d1225feae1d863b1
Peter <shadman48@gmail.com>
2017-10-06 21:10:22 -0700

    Git Init, Created Ex2Client

---
+++ Ex2Client.java
@@ -0,0 +1,15 @@
+import java.net.Socket;
+
+public final class Ex2Client 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+        
+    	try (Socket socket = new Socket("18.221.102.182", 38102)) 
+        {
+    		
+        }
+	}
+
+}
