d7325c72a9b403f7c34190b03237f12d8a9e0d4b
Peter <shadman48@gmail.com>
2017-10-11 22:12:28 -0700

    FINNALY WORKING, now to clean up the code.

--- Ex2Client.java
+++ Ex2Client.java
@@ -20,60 +20,82 @@ public final class Ex2Client
     		// Read from INPUTSTREAM twice then concat bytes.
     		InputStream is = socket.getInputStream();
     		DataInputStream dis = new DataInputStream(is);
+    		OutputStream out = socket.getOutputStream(); 
+    	    DataOutputStream dos = new DataOutputStream(out);
+    	    
     		byte[] byteArrayIn1 = new byte[100];
     		byte[] byteArrayIn2 = new byte[100];
     		byte[] byteArrayOut = new byte[100];
     		byte[] byteArrayReturn = new byte[1];
-    		byteArrayReturn[0] = 1;
     		
-    		dis.readFully(byteArrayIn1);
-    		dis.readFully(byteArrayIn2);
     		
-    		int n = 0;
+//    		dis.readFully(byteArrayIn1);
+//    		dis.readFully(byteArrayIn2);
+    		
+    		
     		byte tempByte;
     		
     		//byte[] test = Files.readAllBytes(new File("/fileToRead.txt").toPath());
 //    		Path fileLocation = Paths.get("fileToRead.txt");
 //    		byte[] test = Files.readAllBytes(fileLocation);
-    		String s="51E0908A32F6867BD5DF384AC3A040D75D699CCB9B137545AEA15BA078C7C0FAF205BA90E5A4C7C95DC2358DD7DC89CBBBA408933D13C39B3945716F153C1840E77C184D0A60661BBF20FC4C9F0BFB043C0990437604AE6BC60102483A127DBC7220BD84";
+    		//String s="51E0908A32F6867BD5DF384AC3A040D75D699CCB9B137545AEA15BA078C7C0FAF205BA90E5A4C7C95DC2358DD7DC89CBBBA408933D13C39B3945716F153C1840E77C184D0A60661BBF20FC4C9F0BFB043C0990437604AE6BC60102483A127DBC7220BD84";
     		 
-    		byte[] test = new BigInteger(s,16).toByteArray();
+    		//byte[] test = new BigInteger(s,16).toByteArray();
     		
-    		for(int i = 0; i <= byteArrayOut.length - 1; n++)
+    		for(int i = 0; i <= byteArrayOut.length - 1; i++)
     		{
-    			tempByte = twoBytesToShort(byteArrayIn1[n], byteArrayIn2[n]);
+    			byteArrayIn1[i] = (byte) dis.readUnsignedByte();
+    			byteArrayIn2[i] = (byte) dis.readUnsignedByte();
+    			
+    			tempByte = twoBytesToShort(byteArrayIn1[i], byteArrayIn2[i]);
     			byteArrayOut[i] = tempByte;
-    			i++;
     		}
     		
-    		System.out.println("Hex-form: " + (bytesToHex(test)));
+    		System.out.println("Hex-form: " + (bytesToHex(byteArrayOut)));
 			
     		
     		// use java crc32
     		CRC32 crc32 = new CRC32();
-    		crc32.update(test);
+    		crc32.update(byteArrayOut);
     		System.out.printf("%X\n", crc32.getValue());
     		long bytee = crc32.getValue();
     		
+    		
     		byte [] bytes = ByteBuffer.allocate(8).putLong(bytee).array();
      	    System.out.println(Arrays.toString(bytes));
-    		
+     	    
+     	    
+     	    byte [] bytess = new byte[4];
+     	    bytess[0] = bytes[4];
+     	    bytess[1] = bytes[5];
+     	   	bytess[2] = bytes[6];
+     	  	bytess[3] = bytes[7];
+    	    System.out.println(Arrays.toString(bytess) + "\n" + bytesToHex(bytess));
+   		
      	    
      	    
     		// send crc as sequence back to server
-    		OutputStream out = socket.getOutputStream(); 
-    	    DataOutputStream dos = new DataOutputStream(out);
-    	    
-    	    dos.write(bytes, 4, 4);
-    	    
+    		
+    	  //  dos.write(bytess);
+    	    int temp;
+    	    for(int i = 0; i < 4; i++)
+    	    {
+    	    	temp = unsigned(bytess[i]);
+    	    	bytess[i] = (byte)temp;
+    	    	dos.writeByte(bytess[i]);
+    	    	System.out.println(bytess[i]);
+    	   		
+    	    }
 
     		
     		// if server gets same crc then sends byte = 1 else 0
-    		dis.readFully(byteArrayReturn);
+    		//dis.readFully(byteArrayReturn);
     		
-    		String correct = (bytesToHex(byteArrayReturn)).substring(1);
+    		//String correct = (bytesToHex(byteArrayReturn)).substring(1);
+    		int correct = 0;
+    		correct = dis.readByte();
     		
-    		if(correct == "1")
+    		if(correct == 1)
     			System.out.println("Response Good."); 
     		else
     			System.out.println("Response Bad."); 
@@ -90,9 +112,18 @@ public final class Ex2Client
 	
 	
 	
+	public static int unsigned(byte b) 
+	{
+		return b & 0xff;
+		
+	}
+	
+	
+	
+	
 	public static byte twoBytesToShort(byte b1, byte b2) 
 	{
-		return (byte) ((b1 << 4) | (b2));
+		return (byte) (((b1 & 0xF) << 4) | (b2 & 0xF));
 	}
 	
 	
