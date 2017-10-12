d25625dd47be0b22168305b20c7e274bb657fd14
Peter <shadman48@gmail.com>
2017-10-11 22:47:23 -0700

    Code COMPLETED. clean up all the old comments and added better descriptions. cleaned up old code or commented out code. all works well and is done.

--- Ex2Client.java
+++ Ex2Client.java
@@ -3,10 +3,8 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.math.BigInteger;
 import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.zip.CRC32;
 
 public final class Ex2Client 
@@ -17,7 +15,10 @@ public final class Ex2Client
 		// Create socket
     	try (Socket socket = new Socket("18.221.102.182", 38102)) 
         {
-    		// Read from INPUTSTREAM twice then concat bytes.
+    		
+    		System.out.println("Connected to server.");
+			
+    		
     		InputStream is = socket.getInputStream();
     		DataInputStream dis = new DataInputStream(is);
     		OutputStream out = socket.getOutputStream(); 
@@ -26,22 +27,10 @@ public final class Ex2Client
     		byte[] byteArrayIn1 = new byte[100];
     		byte[] byteArrayIn2 = new byte[100];
     		byte[] byteArrayOut = new byte[100];
-    		byte[] byteArrayReturn = new byte[1];
     		
     		
-//    		dis.readFully(byteArrayIn1);
-//    		dis.readFully(byteArrayIn2);
-    		
-    		
+    		//Reads in the two parts of the byte, then "concats" them togeather. 
     		byte tempByte;
-    		
-    		//byte[] test = Files.readAllBytes(new File("/fileToRead.txt").toPath());
-//    		Path fileLocation = Paths.get("fileToRead.txt");
-//    		byte[] test = Files.readAllBytes(fileLocation);
-    		//String s="51E0908A32F6867BD5DF384AC3A040D75D699CCB9B137545AEA15BA078C7C0FAF205BA90E5A4C7C95DC2358DD7DC89CBBBA408933D13C39B3945716F153C1840E77C184D0A60661BBF20FC4C9F0BFB043C0990437604AE6BC60102483A127DBC7220BD84";
-    		 
-    		//byte[] test = new BigInteger(s,16).toByteArray();
-    		
     		for(int i = 0; i <= byteArrayOut.length - 1; i++)
     		{
     			byteArrayIn1[i] = (byte) dis.readUnsignedByte();
@@ -51,47 +40,38 @@ public final class Ex2Client
     			byteArrayOut[i] = tempByte;
     		}
     		
-    		System.out.println("Hex-form: " + (bytesToHex(byteArrayOut)));
+    		System.out.println("Recived byted: " + (bytesToHex(byteArrayOut)));
 			
     		
-    		// use java crc32
+    		
+    		
+    		//Using java's CRC32.
     		CRC32 crc32 = new CRC32();
     		crc32.update(byteArrayOut);
-    		System.out.printf("%X\n", crc32.getValue());
+    		System.out.printf("Generated CRC32: %X\n", crc32.getValue());
     		long bytee = crc32.getValue();
     		
     		
+    		//converting long to bytes, then only taking non 0 values to new array.
     		byte [] bytes = ByteBuffer.allocate(8).putLong(bytee).array();
-     	    System.out.println(Arrays.toString(bytes));
-     	    
-     	    
      	    byte [] bytess = new byte[4];
      	    bytess[0] = bytes[4];
      	    bytess[1] = bytes[5];
      	   	bytess[2] = bytes[6];
      	  	bytess[3] = bytes[7];
-    	    System.out.println(Arrays.toString(bytess) + "\n" + bytesToHex(bytess));
-   		
-     	    
+    	    
      	    
     		// send crc as sequence back to server
-    		
-    	  //  dos.write(bytess);
     	    int temp;
     	    for(int i = 0; i < 4; i++)
     	    {
     	    	temp = unsigned(bytess[i]);
     	    	bytess[i] = (byte)temp;
     	    	dos.writeByte(bytess[i]);
-    	    	System.out.println(bytess[i]);
-    	   		
     	    }
 
     		
-    		// if server gets same crc then sends byte = 1 else 0
-    		//dis.readFully(byteArrayReturn);
-    		
-    		//String correct = (bytesToHex(byteArrayReturn)).substring(1);
+    		//Reads reply from server,if server gets same crc value then it sends byte = 1 else = 0.
     		int correct = 0;
     		correct = dis.readByte();
     		
@@ -100,6 +80,7 @@ public final class Ex2Client
     		else
     			System.out.println("Response Bad."); 
         }
+    	
     	catch(IOException e)
     	{
     		return;
@@ -111,7 +92,11 @@ public final class Ex2Client
 	
 	
 	
-	
+	/**
+	 * This function applies a mask to a byte.
+	 * @param b
+	 * @return masked byte
+	 */
 	public static int unsigned(byte b) 
 	{
 		return b & 0xff;
@@ -120,7 +105,12 @@ public final class Ex2Client
 	
 	
 	
-	
+	/**
+	 * This function takes in 2 bytes and concatenates them together.
+	 * @param b1 first byte input
+	 * @param b2 second byte input
+	 * @return The concatenation of the two on binary level as a byte.
+	 */
 	public static byte twoBytesToShort(byte b1, byte b2) 
 	{
 		return (byte) (((b1 & 0xF) << 4) | (b2 & 0xF));
@@ -131,7 +121,10 @@ public final class Ex2Client
 
 	
 	
-	
+	/**
+	 * Converts bytes to Hex. 
+	 * Returns string with Hex values.
+	 */
 	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	public static String bytesToHex(byte[] bytes) 
 	{
